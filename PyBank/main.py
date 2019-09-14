c023dd1c65de13731015847dac31c1092f0a5aca
congntran92 <54002262+congntran92@users.noreply.github.com>
2019-09-14 16:06:27 -0700

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,11 +2,10 @@ import os
 import csv
 
 budget_data = os.path.join("budget_data.csv")
-
-total_months = 0
-total_pl = 0
 value = 0
 change = 0
+total_months = 0
+total_pl = 0
 dates = []
 profits = []
 
